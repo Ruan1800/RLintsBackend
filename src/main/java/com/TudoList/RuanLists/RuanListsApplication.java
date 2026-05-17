426b87c68676399abe25dd46577816f9f310f1d5
Ruan Ribeiro <rrjalfim@gmail.com>
2026-05-17 14:40:38 -0300

    first commit

---
+++ src/main/java/com/TudoList/RuanLists/RuanListsApplication.java
@@ -0,0 +1,13 @@
+package com.TudoList.RuanLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuanListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuanListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TudoList/RuanLists/RuanListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TudoList.RuanLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuanListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
